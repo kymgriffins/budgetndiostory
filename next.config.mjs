b4a2b7f1b20fad593f435bc562a3f0b15cbf1f13
kymgriffins <kimutaigriffins1960@gmail.com>
2026-01-28 16:29:26 +0300

    yap

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    localPatterns: [
+      {
+        pathname: '/_next/static/media/.*',
+        search: '^\\?.*',
+      },
+      {
+        pathname: '/.*',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
