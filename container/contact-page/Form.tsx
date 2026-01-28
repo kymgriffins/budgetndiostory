87c7cf5fa6df8e0c48496244cdf8627c0c990625
kymgriffins <kimutaigriffins1960@gmail.com>
2026-01-28 23:14:19 +0300

    form updated

--- container/contact-page/Form.tsx
+++ container/contact-page/Form.tsx
@@ -1,3 +1,4 @@
+
 import Link from "next/link";
 import { RoundButton } from "@/components";
 
@@ -9,141 +10,145 @@ export default function Form() {
 					<div className="flex gap-[10px] w-[50%] sm:w-full xm:w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								Hi! My name is
+								Hello, my name is
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Enter your name*"
-								className="paragraph w-full font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out sm:w-full xm:w-full"
+								placeholder="Your full name*"
+								className="paragraph w-full font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out"
 							/>
 						</div>
 					</div>
+
 					<div className="flex gap-[10px] w-[50%] sm:w-full xm:w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								and I work with
+								and I’m part of
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Compony name type here*"
-								className="paragraph w-full font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out sm:w-full xm:w-full"
+								placeholder="Youth group, NGO, institution, or collective*"
+								className="paragraph w-full font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out"
 							/>
 						</div>
 					</div>
 				</div>
+
 				<div className="w-full flex gap-[10px]">
 					<div className="flex gap-[10px] w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								I’m looking for a partner to help me with
+								We’re exploring a partnership around
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Your goal type here*"
-								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out w-full sm:w-full xm:w-full"
+								placeholder="Youth engagement, budget transparency, data storytelling, policy work*"
+								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out w-full"
 							/>
 						</div>
 					</div>
 				</div>
+
 				<div className="w-full flex gap-[10px]">
 					<div className="flex gap-[10px] w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								With an idea of having that completed
+								with a target timeline of
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Date*"
-								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out w-full sm:w-full xm:w-full"
+								placeholder="Proposed timeline or milestone*"
+								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out w-full"
 							/>
 						</div>
 					</div>
 				</div>
+
 				<div className="w-full flex gap-[10px]">
 					<div className="flex gap-[10px] w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								I am hoping to stay around a budget range of
+								and an estimated budget of
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Select*"
-								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out w-full sm:w-full xm:w-full"
+								placeholder="Indicative budget range (KES or USD)*"
+								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out w-full"
 							/>
 						</div>
 					</div>
 				</div>
+
 				<div className="w-full flex gap-[10px]">
 					<div className="flex gap-[10px] w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								You can reach me at
+								You can contact me via
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="name@example.com"
-								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out w-full sm:w-full xm:w-full"
+								placeholder="Email address*"
+								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out w-full"
 							/>
 						</div>
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								to start the conversation.
+								to start the discussion.
 							</h2>
 						</div>
 					</div>
 				</div>
+
 				<div className="w-full flex gap-[10px]">
 					<div className="flex gap-[10px] w-full sm:flex-col xm:flex-col">
 						<div className="xl:min-w-max lg:min-w-max md:min-w-max">
 							<h2 className="sub-heading font-NeueMontreal font-normal text-secondry">
-								Optionally, i’m sharing more:
+								Anything else we should know?
 							</h2>
 						</div>
 						<div className="w-full">
 							<input
 								type="text"
-								placeholder="Product details type here..."
-								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transform transition duration-200 ease-in-out w-full sm:w-full xm:w-full"
+								placeholder="Youth audience, data sources, policy focus, or geographic scope (optional)"
+								className="paragraph font-NeueMontreal font-normal text-secondry bg-background border-b border-[#21212155] focus:border-secondry text-center sm:text-left xm:text-left outline-none focus:placeholder:opacity-0 mt-[20px] transition duration-200 ease-in-out w-full"
 							/>
 						</div>
 					</div>
 				</div>
 			</div>
+
 			<div className="w-full flex items-center justify-end sm:justify-start xm:justify-start pt-[50px]">
 				<div className="flex sm:flex-col xm:flex-col gap-[25px]">
 					<div className="flex gap-[10px] items-center">
-						<div className="flex gap-[10px]">
-							<input
-								type="checkbox"
-								className="w-[30px]"
-							/>
-							<p className="paragraph text-secondry font-NeueMontreal font-normal">
-								I agree with the
-							</p>
-						</div>
+						<input type="checkbox" className="w-[30px]" />
+						<p className="paragraph text-secondry font-NeueMontreal font-normal">
+							I have read and agree to the
+						</p>
 						<Link
-							className="paragraph font-medium font-NeueMontreal text-secondry capitalize flex flex-col hover"
-							href={"/privacy"}>
+							className="paragraph font-medium font-NeueMontreal text-secondry capitalize hover"
+							href="/privacy"
+						>
 							Privacy Policy
 						</Link>
 					</div>
-					<div className="w-fit flex items-center justify-between bg-secondry cursor-pointer rounded-full group">
+
+					<div className="w-fit flex items-center justify-between bg-secondry cursor-pointer rounded-full">
 						<RoundButton
 							bgcolor="#212121"
 							href="/"
-							title="send inquiry"
+							title="submit partnership request"
 							className="bg-white text-black"
 							style={{ color: "#fff" }}
 						/>
@@ -153,3 +158,4 @@ export default function Form() {
 		</section>
 	);
 }
+
